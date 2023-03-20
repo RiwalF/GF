b01bad837ee43ee1be6162097bfd96828e2d554c
RiwalF <riwal2003.15@gmail.com>
2023-03-20 11:01:46 +0100

    logique

--- GF3/GF3.php
+++ GF3/GF3.php
@@ -62,6 +62,8 @@ $montant = SQLget("SELECT fichefrais.id FROM fichefrais,etat WHERE idVisiteur IN
             <thead>
                 <tr>
                     <th><strong>Fiche de frais de : <?php echo $nom_prenom_visiteur[0]; ?></strong></th>
+                    <th></th>
+                    <th></th>
                     <th><strong>Ajouter</strong><a href="../GF4/GF4_A.php?id=<?php echo $id?>"><img class="pictureCenter" src="https://annuaire-opticien.essilor.fr/media/Picto_plus.png" width="50px"></a></th>
                 </tr>
                 <tr>
@@ -72,9 +74,66 @@ $montant = SQLget("SELECT fichefrais.id FROM fichefrais,etat WHERE idVisiteur IN
                 </tr>
             </thead>
             <tbody>
-                <tr>
 
-                </tr>
+
+<?php                
+$fichefrais = SQLobject("SELECT * FROM `fichefrais` WHERE `idVisiteur`= '$id';");
+
+for ($i=0; $i < count($fichefrais); $i++) 
+{   
+    $mois = $fichefrais[$i][2];
+    $annee = $fichefrais[$i][3];
+    $montant = $fichefrais[$i][5];
+    $idEtat = $fichefrais[$i][7];
+    $idEtat = SQLgetval("SELECT libelle FROM etat WHERE id = '$idEtat';")
+?>
+
+    <tr class="fondBlanc">
+        
+        <th>
+
+            <?php print($fichefrais[$i][2] . "/" . $fichefrais[$i][3] . " " . $fichefrais[$i][5] . " " . $fichefrais[$i][7]); ?>
+
+        </th>
+        <?php                   
+            if ($idEtat==SQLgetval("SELECT libelle FROM etat WHERE id = 'CR';"))
+            {
+        ?>
+        <th title="Supprimer">
+                <a href="GF3_DELETE.php?id=<?php echo $id;?>&mois=<?php echo $mois;?>&annee=<?php echo $annee;?>">
+                    <img class="pictureCenter" src="https://us.123rf.com/450wm/oliveradesign/oliveradesign1812/oliveradesign181200007/126817569-ic%C3%B4ne-ouverte-poubelle-isol%C3%A9-sur-fond-blanc-illustration-vectorielle-.jpg?ver=6">
+                </a>
+        </th>
+
+        <th title="Modifier">
+                <a href="../GF4/GF4_C.php?id=<?php echo $id;?>&mois=<?php echo $mois;?>&annee=<?php echo $annee;?>">
+                    <img class="pictureCenter" src="https://us.123rf.com/450wm/alekseyvanin/alekseyvanin1708/alekseyvanin170800346/83846475-document-avec-stylo-ic%C3%B4ne-de-contour-rempli-signe-de-vecteur-ligne-pictogramme-color%C3%A9-lin%C3%A9aire-isol%C3%A9.jpg">
+                </a>
+        </th>
+        <?php
+            }else{
+        ?>
+            <th title="Supprimer">
+            </th>
+            <th title="Modifier">
+            </th>
+        <?php        
+            }
+        ?>
+
+        <th title="Voir">
+            
+            <a href="../GF5/GF5.php ? id=<?php echo $id; ?>& mois=<?php echo $mois; ?>& annee=<?php echo $annee; ?>">
+                <img class="pictureCenter" src="https://us.123rf.com/450wm/alekseyvanin/alekseyvanin1704/alekseyvanin170401467/75473998-vecteur-d-ic%C3%B4ne-oeil-illustration-de-logo-solide-vision-pictogramme-isol%C3%A9-sur-blanc.jpg?ver=6">
+            </a>
+        
+        </th>
+
+    </tr>
+
+<?php
+}                
+?>
             </tbody>
         </table>
         
@@ -86,66 +145,7 @@ $montant = SQLget("SELECT fichefrais.id FROM fichefrais,etat WHERE idVisiteur IN
 
 
 
-                </tr>
-
-                <?php                
-                $fichefrais = SQLobject("SELECT * FROM `fichefrais` WHERE `idVisiteur`= '$id';");
-
-                for ($i=0; $i < count($fichefrais); $i++) 
-                {   
-                    $mois = $fichefrais[$i][2];
-                    $annee = $fichefrais[$i][3];
-                    $montant = $fichefrais[$i][5];
-                    $idEtat = $fichefrais[$i][7];
-                    $idEtat = SQLgetval("SELECT libelle FROM etat WHERE id = '$idEtat';")
-                ?>
-
-                    <tr class="fondBlanc">
-                        
-                        <th>
-
-                            <?php print($fichefrais[$i][2] . "/" . $fichefrais[$i][3] . " " . $fichefrais[$i][5] . " " . $fichefrais[$i][7]); ?>
-
-                        </th>
-                        <?php                   
-                            if ($idEtat==SQLgetval("SELECT libelle FROM etat WHERE id = 'CR';"))
-                            {
-                        ?>
-                        <th title="Supprimer">
-                                <a href="GF3_DELETE.php?id=<?php echo $id;?>&mois=<?php echo $mois;?>&annee=<?php echo $annee;?>">
-                                    <img class="pictureCenter" src="https://us.123rf.com/450wm/oliveradesign/oliveradesign1812/oliveradesign181200007/126817569-ic%C3%B4ne-ouverte-poubelle-isol%C3%A9-sur-fond-blanc-illustration-vectorielle-.jpg?ver=6">
-                                </a>
-                        </th>
-
-                        <th title="Modifier">
-                                <a href="../GF4/GF4_C.php?id=<?php echo $id;?>&mois=<?php echo $mois;?>&annee=<?php echo $annee;?>">
-                                    <img class="pictureCenter" src="https://us.123rf.com/450wm/alekseyvanin/alekseyvanin1708/alekseyvanin170800346/83846475-document-avec-stylo-ic%C3%B4ne-de-contour-rempli-signe-de-vecteur-ligne-pictogramme-color%C3%A9-lin%C3%A9aire-isol%C3%A9.jpg">
-                                </a>
-                        </th>
-                        <?php
-                            }else{
-                        ?>
-                            <th title="Supprimer">
-                            </th>
-                            <th title="Modifier">
-                            </th>
-                        <?php        
-                            }
-                        ?>
-
-                        <th title="Voir">
-                            
-                            <a href="../GF5/GF5.php ? id=<?php echo $id; ?>& mois=<?php echo $mois; ?>& annee=<?php echo $annee; ?>">
-                                <img class="pictureCenter" src="https://us.123rf.com/450wm/alekseyvanin/alekseyvanin1704/alekseyvanin170401467/75473998-vecteur-d-ic%C3%B4ne-oeil-illustration-de-logo-solide-vision-pictogramme-isol%C3%A9-sur-blanc.jpg?ver=6">
-                            </a>
-                        
-                        </th>
-
-                    </tr>
-
-                <?php
-                }                
-                ?>
+               
 
             </table>
         
