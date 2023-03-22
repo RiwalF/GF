3ed6b3fe15662f9de1c1978ac1b683d72f1eb9c1
RiwalF <riwal2003.15@gmail.com>
2023-03-22 13:07:44 +0100

    lien

--- GFC.php
+++ GFC.php
@@ -20,18 +20,11 @@ ini_set("display_errors", 1);
             </table>
 
             <fieldset>
-                <label class="titre2">Validation des Frais <input type="submit"/></label></br></br>
+            <input type="button" name="lien1" value="nom du lien" onclick="self.location.href='GF6/GF6.php'" style="background-color:#3cb371" style="color:white; font-weight:bold"onclick>
+            
+            <label class="titre2">Validation des Frais <input type="submit"/></label></br></br>
                 <a href="GF7/GF7.php"><label class="PAIEMENT">Mettre en paiement</label>
             </fieldset>
-            <?php
-            if ($ExisteFicheFrais) {
-            ?>
-                <input id="mois" name="mois" type="hidden" value="<?php echo $mois; ?>">
-                <input id="an" name="an" type="hidden" value="<?php echo $annee; ?>">
-                <input id="id" name="id" type="hidden" value="<?php echo $id; ?>">
-            <?php
-            }
-            ?>
         </form>
     </body>
 </html>
