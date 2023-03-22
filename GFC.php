5004c6aae014311f46e98e524240711f15a28d92
RiwalF <riwal2003.15@gmail.com>
2023-03-22 09:20:48 +0100

    aie

--- GFC.php
+++ GFC.php
@@ -13,7 +13,7 @@ function SQLget($sql){
 
 $ExisteFicheFrais = TRUE;
 $tab_id = SQLget("SELECT DISTINCT visiteur.id,annee,mois FROM visiteur,fichefrais WHERE fichefrais.idVisiteur=visiteur.id AND idEtat='CL';");
-if ($tab_id[0] != NULL) {
+if ($tab_id != NULL) {
     $id = $tab_id[0];
     $annee = $tab_id[1];
     $mois = $tab_id[2];   
