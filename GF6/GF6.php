0b0b824cdf258b1f5d8865960bc0ac3f443617d8
RiwalF <riwal2003.15@gmail.com>
2023-04-01 11:46:04 +0200

    classe back-button

--- GF6/GF6.php
+++ GF6/GF6.php
@@ -46,7 +46,7 @@ $tab_id = SQLget("SELECT DISTINCT visiteur.id,annee,mois FROM visiteur,fichefrai
             <fieldset class="orange">
                 <h1 class="titre2">Validation des Frais des visiteur</h1>
                     <br/>
-                    <input type="button" name="lien0" value="Retour en arriere" onclick="self.location.href='../GFC.php'" style="background-color:#658db3" style="color:white; font-weight:bold"onclick>
+                    <input class="back-button" type="button" name="lien0" value="Retour en arriere" onclick="self.location.href='../GFC.php'" style="background-color:#658db3" style="color:white; font-weight:bold"onclick>
                     <br/>
                 <br />
                     <select id="ficheFraisList" name="idFicheFrais">
