2548ed0f429b549b8881d14d5be6b4c9eb467491
RiwalF <riwal2003.15@gmail.Com>
2023-05-22 09:58:03 +0200

    jdrf

--- GSB_RIWAL/GSB_Connexions.php
+++ GSB_RIWAL/GSB_Connexions.php
@@ -7,15 +7,8 @@
     </head>    
     <body>  
         <form name="GF4/GF4.php" action="Choix_Connexions.php" method="get">
-            <table>
-                <tr>
-                    <th></th>
-                </tr>
-            </table>
-
             <fieldset>
                 <h1 class="titre2">Connexion</h1>
-                <h1 class="titre">Saisie</h1><BR>
                 <label for="id" class="titre2">Identifiant : </label><input type="text" id="id" name="id" placeholder="AAAA" />
             </fieldset>
         </form>    
