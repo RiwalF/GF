3ca02fa5d1c4dacdd2cd8b7f69dd38771f7c17bd
RiwalF <riwal2003.15@gmail.Com>
2023-05-22 15:57:01 +0200

    submit

--- GSB_RIWAL/GSB_Connexions.php
+++ GSB_RIWAL/GSB_Connexions.php
@@ -6,13 +6,17 @@
 
     </head>    
     <body>  
-        <form name="GF4/GF4.php" action="Choix_Connexions.php" method="get">
-            <fieldset class="orange">
-                <h1 class="titre2">Connexion</h1>
-                <label for="id" class="titre2">Identifiant : </label><input type="text" id="id" name="id" placeholder="AAAA" class="my-text-input" />
-                <label for="mdp" class="titre2">Mot de passe : </label><input type="password" id="mdp" name="mdp" placeholder="**********" class="my-text-input" />
-            </fieldset>
-        </form>    
+    <form name="GF4/GF4.php" action="Choix_Connexions.php" method="get">
+        <fieldset class="orange">
+            <h1 class="titre2">Connexion</h1>
+            <label for="id" class="titre2">Identifiant :</label>
+            <input type="text" id="id" name="id" placeholder="AAAA" class="my-text-input" />
+            <label for="mdp" class="titre2">Mot de passe :</label>
+            <input type="password" id="mdp" name="mdp" placeholder="**********" class="my-text-input" />
+            <input type="submit" value="Submit" style="background-color:#658db3" style="color:white; font-weight:bold"onclick/>
+        </fieldset>
+    </form>
+
 
         <div class="alert" id="alert-box">
             <div class="ball"></div>
