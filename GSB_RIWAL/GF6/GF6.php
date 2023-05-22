11d87a98697896788aeaabce99d923979aee861d
RiwalF <riwal2003.15@gmail.Com>
2023-05-22 09:38:29 +0200

    cssss

--- GSB_RIWAL/GF6/GF6.php
+++ GSB_RIWAL/GF6/GF6.php
@@ -38,7 +38,7 @@ $tab_id = SQLget("SELECT DISTINCT visiteur.id,annee,mois FROM visiteur,fichefrai
     <head>
         <title>Suivi de remboursement des Frais</title>
         <meta charset="utf-8">
-		<link type="text/css" rel="stylesheet" href="GF.css" />
+		<link type="text/css" rel="stylesheet" href="../GF.css" />
 
     </head>    
     <body>
