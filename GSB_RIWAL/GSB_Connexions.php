b590ba59c14ff55d2885e11d90eda1391cf20cc2
RiwalF <riwal2003.15@gmail.Com>
2023-05-22 10:21:14 +0200

    erg

--- GSB_RIWAL/GSB_Connexions.php
+++ GSB_RIWAL/GSB_Connexions.php
@@ -9,7 +9,7 @@
         <form name="GF4/GF4.php" action="Choix_Connexions.php" method="get">
             <fieldset>
                 <h1 class="titre2">Connexion</h1>
-                <label for="id" class="titre2">Identifiant : </label><input type="text" id="id" name="id" placeholder="AAAA" />
+                <label for="id" class="titre2">Identifiant : </label><input type="text" id="id" name="id" placeholder="AAAA" class="my-text-input" />
             </fieldset>
         </form>    
     </body>
