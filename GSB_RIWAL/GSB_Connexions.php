b05c9f4edf1df2edd3fc875647d410c0ef015986
RiwalF <riwal2003.15@gmail.Com>
2023-05-22 09:57:28 +0200

    connexion

--- GSB_RIWAL/GSB_Connexions.php
+++ GSB_RIWAL/GSB_Connexions.php
@@ -2,20 +2,20 @@
     <head>
         <title>Connexion au Formulaire de paiement</title>
         <meta charset="utf-8">
-		<link type="text/css" rel="stylesheet" href="GF4/GF4.css" />
+		<link type="text/css" rel="stylesheet" href="GF.css" />
 
     </head>    
     <body>  
         <form name="GF4/GF4.php" action="Choix_Connexions.php" method="get">
             <table>
                 <tr>
-                    <th><h1 class="titre">Connexion</h1></th>
-                    <th><img src="GF4/GSB.jpg"></th>
+                    <th></th>
                 </tr>
             </table>
 
             <fieldset>
-                <h1 class="titre2">Saisie</h1><BR>
+                <h1 class="titre2">Connexion</h1>
+                <h1 class="titre">Saisie</h1><BR>
                 <label for="id" class="titre2">Identifiant : </label><input type="text" id="id" name="id" placeholder="AAAA" />
             </fieldset>
         </form>    
