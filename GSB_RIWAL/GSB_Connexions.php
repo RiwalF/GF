e6a36f1068b14c458e13f0043238cbe980a5b24a
RiwalF <riwal2003.15@gmail.Com>
2023-05-22 15:57:29 +0200

    efgr

--- GSB_RIWAL/GSB_Connexions.php
+++ GSB_RIWAL/GSB_Connexions.php
@@ -12,7 +12,7 @@
             <label for="id" class="titre2">Identifiant :</label>
             <input type="text" id="id" name="id" placeholder="AAAA" class="my-text-input" />
             <label for="mdp" class="titre2">Mot de passe :</label>
-            <input type="password" id="mdp" name="mdp" placeholder="**********" class="my-text-input" />
+            <input type="password" id="mdp" name="mdp" placeholder="**********" class="my-text-input" /></br>
             <input type="submit" value="Submit" style="background-color:#658db3" style="color:white; font-weight:bold"onclick/>
         </fieldset>
     </form>
