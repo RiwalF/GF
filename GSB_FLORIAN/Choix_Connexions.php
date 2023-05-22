7fb61d886f3fbca6f1fcc8ba5826f61d99a892ce
Florian DOUELLE <96247021+Flow-13@users.noreply.github.com>
2023-05-22 16:24:37 +0200

    Add files via upload

--- GSB_FLORIAN/Choix_Connexions.php
+++ GSB_FLORIAN/Choix_Connexions.php
@@ -3,7 +3,15 @@ ini_set("display_errors", 1);
 include 'mesFonctionsGenerales.php';
 
 $id = $_GET["id"];
+$mdp = $_GET["mdp"];
 
+function SQL($sql)
+{
+    $cnxBDD = connexion();
+
+    $result = $cnxBDD->query($sql)
+        or die ("Requete invalide : ".$sql);
+}
 function SQLget($sql){
     $cnxBDD = connexion();
     $result = $cnxBDD->query($sql)
@@ -11,9 +19,36 @@ function SQLget($sql){
     $valeur = $result->fetch_array();
     return $valeur;
 }
+function SQLobject($sql)
+{
+    $cnxBDD = connexion();
 
-$statut_C = SQLget("SELECT * FROM `comptable` WHERE `Comptable_id` = '$id';");
-$statut_V = SQLget("SELECT * FROM `visiteur` WHERE `id` = '$id';");
+    $result = $cnxBDD->query($sql)
+        or die ("Requete invalide : ".$sql);    
+    $valeur = $result->fetch_all();
+    return $valeur;
+}
+
+$annee_du_jour = date("Y")-2000;
+$mois_du_jour = date("m");
+$fiche_cloture = SQLobject("SELECT `id`, `idEtat`, `mois`, `annee` FROM `fichefrais` WHERE idEtat = 'CR';");
+foreach ($fiche_cloture as $key => $fiche) {
+    if ($fiche[3] < $annee_du_jour) {
+        SQL("UPDATE `fichefrais`
+            SET `idEtat` = 'CL'
+            WHERE `id` = ".$fiche[0].";");
+    }elseif ($fiche[3] == $annee_du_jour) {
+        if ($fiche[2] < $mois_du_jour) {
+            SQL("UPDATE `fichefrais`
+            SET `idEtat` = 'CL'
+            WHERE `id` = '".$fiche[0]."';");
+        }
+    }
+}
+
+
+$statut_C = SQLget("SELECT * FROM `comptable` WHERE `Comptable_id` = '$id' AND `MDP` = '$mdp';");
+$statut_V = SQLget("SELECT * FROM `visiteur` WHERE `id` = '$id' AND `MDP` = '$mdp';");
 
 if (gettype($statut_C) == "array" ){
     header('Location: GFC.php');
@@ -25,22 +60,19 @@ if (gettype($statut_C) == "array" ){
             <head>
                 <title>Connexion au Formulaire de paiement</title>
                 <meta charset='utf-8'>
-                <link type='text/css' rel='stylesheet' href='GF4/GF4.css' />
+                <link type='text/css' rel='stylesheet' href='GF.css' />
 
             </head>    
             <body>  
-                <table>
-                    <tr>
-                        <th><h1 class='titre'>Redirection</h1></th>
-                        <th><img src='GF4/GSB.jpg'></th>
-                    </tr>
-                </table>
-
                 <fieldset>
-                    <h1 class='titre2'>Erreur de connexion veuillez réessayer</h1>
+                    <h1 class='titre'>Erreur de connexion veuillez réessayer</h1>
                 </fieldset>
+                <div class='alert' id='alert-box'>
+                <div class='ball'></div>
+                </div>
+                <script src='../script.js'></script>
             </body>
-        </html>";
+          </html>";
 }
 
 
--- GSB_FLORIAN/GSB_Connexions.php
+++ GSB_FLORIAN/GSB_Connexions.php
@@ -2,22 +2,26 @@
     <head>
         <title>Connexion au Formulaire de paiement</title>
         <meta charset="utf-8">
-		<link type="text/css" rel="stylesheet" href="GF4/GF4.css" />
+		<link type="text/css" rel="stylesheet" href="GF.css" />
 
     </head>    
     <body>  
-        <form name="GF4/GF4.php" action="Choix_Connexions.php" method="get">
-            <table>
-                <tr>
-                    <th><h1 class="titre">Connexion</h1></th>
-                    <th><img src="GF4/GSB.jpg"></th>
-                </tr>
-            </table>
+    <form name="GF4/GF4.php" action="Choix_Connexions.php" method="get">
+        <fieldset class="orange">
+            <h1 class="titre2">Connexion</h1>
+            <label for="id" class="titre2">Identifiant :</label>
+            <input type="text" id="id" name="id" placeholder="AAAA" class="my-text-input" />
+            <label for="mdp" class="titre2">Mot de passe :</label>
+            <input type="password" id="mdp" name="mdp" placeholder="**********" class="my-text-input" /></br>
+            <input type="submit" value="Submit" style="background-color:#658db3" style="color:white; font-weight:bold"onclick/>
+        </fieldset>
+    </form>
 
-            <fieldset>
-                <h1 class="titre2">Saisie</h1><BR>
-                <label for="id" class="titre2">Identifiant : </label><input type="text" id="id" name="id" placeholder="AAAA" />
-            </fieldset>
-        </form>    
+
+        <div class="alert" id="alert-box">
+            <div class="ball"></div>
+        </div>
+        <script src="script.js"></script>
+
     </body>
 </html>
