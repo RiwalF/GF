67d25ec12b32427133cbfb5cef96dffcb1e694aa
RiwalF <riwal2003.15@gmail.Com>
2023-05-22 13:50:49 +0200

    gf7

--- GSB_RIWAL/GF7/GF7.php
+++ GSB_RIWAL/GF7/GF7.php
@@ -27,35 +27,22 @@ SQL("UPDATE `fichefrais` SET `idEtat` = 'RB' WHERE `idEtat` = 'VA';");
 
 <!DOCTYPE html>
 
-<html style = "background-color: rgb(101, 141, 179)">
+        <html>
+            <head>
+                <title>Connexion au Formulaire de paiement</title>
+                <meta charset='utf-8'>
+                <link type='text/css' rel='stylesheet' href='GF.css' />
 
-    <head lang=fr>
+            </head>    
+            <body>  
+                <fieldset>
+                    <h1 class='titre'>Toutes les fiches de frais à l’état "Validée" ont été mises en
+        paiement et sont passées à l’état "Remboursée</h1>
+                </fieldset>
+                <div class="alert" id="alert-box">
+                    <div class="ball"></div>
+                </div>
+                <script src="../script.js"></script>
+            </body>
+        </html>
 
-        <meta charset="utf-8">
-		<link type="text/css" rel="stylesheet" href="../GF.css" />
-        <title>Action réalisée avec succès</title>
-
-    </head>
-
-    <body>
-
-        <p style = "text-align: center ; text-align: middle ; font-size: 50px ; font-family: Calibri ; color: white ;"><br><br><br><strong>Toutes les fiches de frais à l’état "Validée" ont été mises en</br>
-        paiement et sont passées à l’état "Remboursée"</strong></p>
-
-        <form name="GF4" action="../GFC.php" method="get">
-
-            <div>
-
-                <input id="Valider" type="submit" value="Revenir à la redirection" />
-                
-            </div>
-
-        </form>
-        <div class="alert" id="alert-box">
-            <div class="ball"></div>
-        </div>
-        <script src="../script.js"></script>
-
-    </body>
-
-</html>
